5dd85347a47af22e2658e1998e0901bf2029ff01
Juan Ignacio Borda <juanignacioborda@gmail.com>
2018-08-01 13:25:47 -0300

    fix apt.conf path

--- buildc9.sh
+++ buildc9.sh
@@ -5,7 +5,7 @@ git clone https://github.com/c9/core.git
 echo "current dir" $(pwd)
 docker run --rm \
     -v $(pwd)/core:/core \
-    -v $(pwd)/apt.conf:/etc/apt/apt.conf \
+    -v $(pwd)/../apt.conf:/etc/apt/apt.conf \
     -v $(pwd)/root:/root \
     debian:stretch \
     bash -c "apt update && apt install -y curl python2.7 build-essential git && /core/scripts/install-sdk.sh"
